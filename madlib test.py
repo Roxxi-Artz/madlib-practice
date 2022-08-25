4905f1e673e43e505e8766b8747ffc8534857551
Roxxi-Artz <90343355+Roxxi-Artz@users.noreply.github.com>
2022-08-25 08:02:50 -0400

    Add files via upload

---
+++ madlib test.py
@@ -0,0 +1,11 @@
+noun = input("Type a noun here: ")
+
+verb = input("Type a verb ending in -ed here: ")
+
+adjective = input("Type an adjective here: ")
+
+def wowowow(bloob, hloob, shoob):
+    madlib = (f"The {shoob} fox {hloob} over the lazy {bloob}.")
+    print(madlib)
+
+wowowow(noun, verb, adjective)
